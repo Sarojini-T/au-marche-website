ba09b0ab6588704eca1616e8d28e66f26afb79a2
Sarojini <sarojini.torchon@gmail.com>
2023-09-15 12:39:38 -0400

    add: deploy workflow

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/theMarket-react-website/",
+  base: "/react-website/",
   plugins: [react()],
   
 })
